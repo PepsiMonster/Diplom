73b3491f6e30a0d67da7a0ab6190f782f950983b
PepsiMonster <mikecelestialfeind@gmail.com>
2026-03-23 23:13:42 +0300

    ch_1 started

--- tex/build.bat
+++ tex/build.bat
@@ -1,1 +1,16 @@
+@echo off
+cd /d "%~dp0"
+
 lualatex main.tex && biber main && lualatex main.tex && lualatex main.tex
+
+if exist "..\tex.zip" del /f /q "..\tex.zip"
+
+tar -a -c -f "..\tex.zip" ^
+  main.tex ^
+  build.bat ^
+  .gitignore ^
+  bib ^
+  sections ^
+  images
+
+pause
