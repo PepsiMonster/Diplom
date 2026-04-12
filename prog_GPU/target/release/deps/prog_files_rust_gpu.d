6bf677634993a4dd9686a9b2bd87b815604288c9
PepsiMonster <mikecelestialfeind@gmail.com>
2026-04-12 09:54:48 +0300

    gpu iteration success

--- prog_GPU/target/release/deps/prog_files_rust_gpu.d
+++ prog_GPU/target/release/deps/prog_files_rust_gpu.d
@@ -1,10 +1,10 @@
-C:\Users\kotof\Study\Diploma\prog_GPU\target\release\deps\prog_files_rust_gpu.d: src\main.rs src\backend\mod.rs src\backend\gpu.rs src\cli.rs src\experiments.rs src\output.rs src\params.rs src\run.rs src\scenario_grid.rs src\stats.rs
+C:\Users\kotof\Study\Diploma\prog_GPU\target\release\deps\prog_files_rust_gpu.d: src\main.rs src\backend\mod.rs src\backend\cpu_ref.rs src\cli.rs src\experiments.rs src\output.rs src\params.rs src\run.rs src\scenario_grid.rs src\stats.rs
 
-C:\Users\kotof\Study\Diploma\prog_GPU\target\release\deps\prog_files_rust_gpu.exe: src\main.rs src\backend\mod.rs src\backend\gpu.rs src\cli.rs src\experiments.rs src\output.rs src\params.rs src\run.rs src\scenario_grid.rs src\stats.rs
+C:\Users\kotof\Study\Diploma\prog_GPU\target\release\deps\prog_files_rust_gpu.exe: src\main.rs src\backend\mod.rs src\backend\cpu_ref.rs src\cli.rs src\experiments.rs src\output.rs src\params.rs src\run.rs src\scenario_grid.rs src\stats.rs
 
 src\main.rs:
 src\backend\mod.rs:
-src\backend\gpu.rs:
+src\backend\cpu_ref.rs:
 src\cli.rs:
 src\experiments.rs:
 src\output.rs:
